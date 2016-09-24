43c90b5a63ee78b448980a4a33f1bcce94537453
mangadinho <Votre@email.com>
2016-09-25 01:42:25 +0400

    ajout de css

--- index.php
+++ index.php
@@ -2,6 +2,31 @@
           <!DOCTYPE html>
           <html>
           <head>
+		    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+		    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+            <link rel="stylesheet" type="text/css" href="css/style.css">
+            <script src="js/jquery.js"></script>
+		    <script src="js/bootstrap.js"></script>
+		    <script src="js/bootstrap-grid.min.js"></script>
+		    <script>
+		        $(document).ready(function(){
+		            MakeBootstrapGridOverlay({
+		                showByDefault: false,
+		                color: '#5086b8',
+		                opacity: '0.3',
+		                showOuterGuides: true,
+		                outerGuidesColor: '#27506d',
+		                showInnerGuides: true,
+		                innerGuidesColor: '#000000',
+		                showHorizontalLines: true,
+		                horizontalLinesColor: '#000000',
+		                horizontalLinesOpacity: '0.1',
+		                horizontalLinesHeight: '25',
+		                hotkey: 'G'
+		            });
+		        });
+		    </script>
           	<title></title>
           </head>
           <body>
